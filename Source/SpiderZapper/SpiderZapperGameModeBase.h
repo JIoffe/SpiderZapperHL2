fcdadd181afc7a808390b1783b236e88b876dede
Jason Ioffe <JIoffe@users.noreply.github.com>
2021-01-19 19:08:53 -0500

    Initial Commit

---
+++ Source/SpiderZapper/SpiderZapperGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpiderZapperGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPIDERZAPPER_API ASpiderZapperGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
